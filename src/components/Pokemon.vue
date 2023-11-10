daefbb0c7f4de52867b9aa2bc3ef783a0b6f2070
ayoub bellali <ayoub.bellali@student.howest.be>
2023-11-10 15:55:32 +0100

    pokemon namen getoond

---
+++ src/components/Pokemon.vue
@@ -0,0 +1,12 @@
+<template>
+  <p>hallo ik ben {{ pokemonData.name }}</p>
+</template>
+<script setup>
+const props = defineProps({
+  pokemonData: {
+    type: Object, // {}
+    required: true,
+  },
+});
+</script>
+<style></style>
